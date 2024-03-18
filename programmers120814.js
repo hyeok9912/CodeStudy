c488b51cd8464ae513b6162232d667ae71dc0cef
혁 <163282490+hyeok9912@users.noreply.github.com>
2024-03-18 13:18:48 +0900

    피자 나눠 먹기(1) (Lv.1)

---
+++ programmers120814.js
@@ -0,0 +1,34 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 1; i <= n; i++){ // 1~n번 까지 반복
+        if(n % 7 === 0){        // n % 7 === 0 이면 
+             
+            return n/7
+        }else if(n % 7 <= 1){
+             
+            return parseInt(n/7)+1
+        }
+    }
+    
+    
+    return answer;
+}
+
+
+
+//// 다른사람 풀이
+
+function solution (n){
+    if(n % 7 === 0){
+        return n / 7
+    }else {
+       return  parseInt(n / 7) + 1;
+    }
+}
+
+///// 다른사람 풀이
+
+function solution(n) {
+    return Math.ceil(n / 7)
+}
+// Math.ceil (올림 메서드) 를 알고 있었다면 훨신 간결하게 코드를 짤 수 있었을 거 같다

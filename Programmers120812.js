503a2e80626707b27eb100c13658563bc1b153e0
혁 <163282490+hyeok9912@users.noreply.github.com>
2024-03-19 17:07:48 +0900

    최빈값 구하기(Lv.1)

---
+++ Programmers120812.js
@@ -0,0 +1,34 @@
+function solution(array) {
+    var answer = 0;
+    let map = new Map();
+    
+    if(array.length === 1){
+        return array[0]
+    }
+    
+    
+    /// map 객체 초기화
+    for(let i = 0; i <= Math.max(...array); i++){
+        map.set(i,0)
+    }
+    for(let i = 0; i < array.length; i++){
+        map.set(array[i],map.get(array[i])+1)
+    }
+    
+    const arr = Array.from(map.values())
+    const max = Math.max(...arr)
+    
+    
+    // 최대값이 하나만 있다면 max, 여러개 중복된다면 -1 출력
+    
+    if(arr.indexOf(max) !== arr.lastIndexOf(max)){
+        return -1
+    } else{
+        return arr.indexOf(max)
+    }
+    
+    
+    
+    
+    return answer;
+}

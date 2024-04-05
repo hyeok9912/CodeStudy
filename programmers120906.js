07748257c2ca9a964c9440a7e17e925f522fb276
혁 <163282490+hyeok9912@users.noreply.github.com>
2024-04-05 11:24:07 +0900

    자릿수 더하기(Lv.1)

---
+++ programmers120906.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    let str = String(n)
+    for(let i = 0; i < str.length; i++){
+        answer += parseInt(str[i])
+    }
+    
+    
+    
+    return answer;
+}

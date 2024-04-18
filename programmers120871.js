9425f6adb7856cf3af4c5ebd9ebb67ca37b22429
혁 <163282490+hyeok9912@users.noreply.github.com>
2024-04-18 18:14:35 +0900

    저주의 숫자 3 (Lv.1)

---
+++ programmers120871.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 1; i <= n; i++){
+        if(i % 3 === 0 || String(i).includes('3')){
+            n +=1
+        }
+
+    }
+    
+    
+    
+    return n;
+} 
